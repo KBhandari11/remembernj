d68da94dc5e83cc57f78b6694cb9d3f04e10b6d7
KBhandari11 <32970056+KBhandari11@users.noreply.github.com>
2019-03-12 14:37:26 -0400

    initial commit

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { browserHistory } from 'react-router';
-import { elastic as Menu } from 'react-burger-menu';
+import { reveal as Menu } from 'react-burger-menu';
 import HomePage from './HomePage';
 import About from './About';
 import BeforetheWar from './BeforetheWar';
@@ -15,42 +15,42 @@ const routes = [
   {
     path: "/",
     exact: true,
-    sidebar: () =><div class="head"> 1.0 Home</div>,
+    sidebar: () =><div></div>,
     main: () =><Route name="home" exact path="/" component={HomePage} />
   },
   {
     path: "/about",
-    sidebar: () =><div class="head">2.0 About</div>,
+    sidebar: () =><div class="head">About</div>,
     main: () =><Route exact path="/about" component={About} />
   },
   {
     path: "/beforethewar",
-    sidebar: () =><div class="head">3.0 Before the War</div>,
+    sidebar: () =><div class="head">Before the War</div>,
     main: () =><Route exact path="/beforethewar" component={BeforetheWar} />
   },
   {
     path: "/kristallnacht",
-    sidebar: () =><div class="head">4.0 Kristallnacht</div>,
+    sidebar: () =><div class="head">Kristallnacht</div>,
     main: () =><Route exact path="/kristallnacht" component={Kristallnacht} />
   },
   {
     path: "/stories",
-    sidebar: () =><div class="head">5.0 Stories</div>,
+    sidebar: () =><div class="head">Stories</div>,
     main: () =><Route exact path="/stories" component={Stories} />
   },
   {
     path: "/peter",
-    sidebar: () =><div class="head">5.0.1 Peter Lederman</div>,
+    sidebar: () =><div class="head">Peter Lederman</div>,
     main: () =><Route exact path="/peter" component={Peter} />
   },
   {
     path: "/bea",
-    sidebar: () =><div class="head">5.0.2 Bea Muhlfelder</div>,
+    sidebar: () =><div class="head">Bea Muhlfelder</div>,
     main: () =><Route exact path="/bea" component={Bea} />
   },
   {
     path: "/timeline",
-    sidebar: () => <div class="head">6.0 Timeline</div>,
+    sidebar: () => <div class="head">Timeline</div>,
     main: () =><Route exact path="/timeline" component={Timeline} />
   }
 ];
@@ -61,66 +61,64 @@ class App extends Component {
 
   render() {
     return (
-
-
-
-
     <Router>
-
-
       <div class="Page">
-          <div>
-        < NavBar/></div>
-          <div class="flex-container">
-
-                <div class="slide" style={{  width: "15%", height: "20",background: "white"   }}>
-                    {routes.map((route, index) => (
-// You can render a <Route> in as many places
-// as you want in your app. It will render along
-// with any other <Route>s that also match the URL.
-// So, a sidebar or breadcrumbs or anything else
-// that requires you to render multiple things
-// in multiple places at the same URL is nothing
-// more than multiple <Route>s.
-        <Route
-        key={index}
-        path={route.path}
-        exact={route.exact}
-        component={route.sidebar}
-        />
-        ))}
-
-                    <Menu customBurgerIcon={ <img src={require('./Pics/menu.jpg')} />} >
-
-                          <a id="Home" className="menu-item" ><Link to="/">1.0 Home</Link></a>
-                          <a id="About" className="menu-item" ><Link to="/about">2.0 About</Link></a>
-                          <a id="" className="menu-item"><Link to="/beforethewar">3.0 Before the War</Link></a>
-                          <a id="" className="menu-item"><Link to="/kristallnacht">4.0 Kristallnacht</Link></a>
-                          <a id="" className="menu-item"><Link to="/peter">5.0 Stories</Link></a>
-                                <ul>
-                                      <li><a id="" className="menu-item"><Link to="/peter">5.0.1 Peter Lederman</Link></a></li>
-                                      <li><a id="" className="menu-item"><Link to="/bea">5.0.2 Bea Muhlfelder</Link></a></li>
-                                </ul>
-                          <a id="" className="menu-item"><Link to="/timeline">6.0 Timeline</Link></a>
-                          <a onClick={ this.showSettings } className="menu-item--small" >Menu</a>
-                  </Menu>
-
-              </div>
-              <div class="main" style={{background: "white"   }}>
-                    {routes.map((route, index) => (
-                        // Render more <Route>s with the same paths as
-                        // above, but different components this time.
-                        <Route
-                        key={index}
-                        path={route.path}
-                        exact={route.exact}
-                        component={route.main}
-                        />
-                      ))}
-              </div>
+        <div class="header">
+          < NavBar/>
+        </div>
+        <div class="topnav" >
+          <div style={{ width: "auto"}}>
+          <Menu customBurgerIcon={ <img src={require('./Pics/menu.jpg')} />} >
+            <a id="Home" className="menu-item" ><Link to="/">Home</Link></a>
+            <a id="About" className="menu-item" ><Link to="/about">About</Link></a>
+            <a id="" className="menu-item"><Link to="/beforethewar">Before the War</Link></a>
+            <a id="" className="menu-item"><Link to="/kristallnacht">Kristallnacht</Link></a>
+            <a id="" className="menu-item"><Link to="/peter">Stories</Link></a>
+                  <ul>
+                        <li><a id="" className="menu-item--small"><Link to="/peter">Peter Lederman</Link></a></li>
+                        <li><a id="" className="menu-item--small"><Link to="/bea">Bea Muhlfelder</Link></a></li>
+                  </ul>
+            <a id="" className="menu-item"><Link to="/timeline"> Timeline</Link></a>
+          </Menu>
           </div>
-          <div style={{ width: "100%"}}>  <Footer/> </div>
+          <div style={{ position:"relative",right:"400px"}}>
+          {routes.map((route, index) => (
+            // You can render a <Route> in as many places
+            // as you want in your app. It will render along
+            // with any other <Route>s that also match the URL.
+            // So, a sidebar or breadcrumbs or anything else
+            // that requires you to render multiple things
+            // in multiple places at the same URL is nothing
+            // more than multiple <Route>s.
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                component={route.sidebar}
+              />
+          ))}
+          </div>
+        </div>
+        <div class="column">
+          {routes.map((route, index) => (
+              // Render more <Route>s with the same paths as
+              // above, but different components this time.
+              <Route
+              key={index}
+              path={route.path}
+              exact={route.exact}
+              component={route.main}
+              />
+            ))}
+        </div>
+
+        <div style={{ width: "100%"}}>
+            <Footer/>
+        </div>
       </div>
+
+
+
     </Router>
     )
   }
